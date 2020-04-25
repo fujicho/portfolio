64cf9853420bd9eaaff2ce0bc7572ab9d44b2c7f
fujicho <nnnn.sohei.fujita@gmail.com>
2020-04-25 16:26:06 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,13 @@
 Rails.application.routes.draw do
+  namespace :teacher do
+    root "top#index"
+  end
+
+  namespace :admin do
+    root "top#index"
+  end
+
+  namespace :student do
+    root "top#index"
+  end
 end
